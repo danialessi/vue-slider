40d731a21825a2876fbb2e42308937bc336aef46
danialessi <daniele.alessi@hotmail.it>
2021-04-20 16:39:37 +0200

    inserito v-for per stampa pallini

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            // images
+            currentImage: 0,
+            imagesPaths: [
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp',
+            ],
+        },
+        methods: {
+            nextImage() {
+                
+            }
         }
     }
 )
